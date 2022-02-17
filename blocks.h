c4e0c1b5fa01f97cf035f3e304d4d80d9ab547a2
Jeroen Bourgois <jeroen@jackjoe.be>
2022-02-17 20:34:18 +0100

    chore: various patches

--- blocks.h
+++ blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "sb-memory",	                    30,		0},
+	{"", "echo '^b#000000^^c#000000^'",      0,   0},
+	{"", "sb-memory",	                       5,		0},
 	{"", "sb-disk /",	                      30,		0},
 	{"", "sb-disk ~/",	                    30,		0},
 	{"", "sb-cpubars",	                     5,		0},
-	{"", "date '+%Y %b %d (%a) %I:%M%p'",    5,		0},
+	{"", "sb-date",                          1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
